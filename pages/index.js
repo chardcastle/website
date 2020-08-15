21bd05d550074f9b80f59bc877af0075256c15b3
Chris Hardcastle <me@chrishardcastle.co.uk>
2020-08-15 20:26:25 +0100

    Migrated head HTML

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,39 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={constants.SITE_DESCRIPTION} />
+        <meta name="author" content={constants.SITE_AUTHOR} />
+
+        <meta property="og:title" content={constants.OG_TITLE} />
+        <meta property="og:type" content={constants.OG_TYPE} />
+        <meta property="og:url" content={constants.OG_URL} />
+        <meta property="og:image" content={constants.OG_IMAGE} />
+        <!-- Optional open graph meta -->
+        <meta property="og:description" content={constants.SITE_DESCRIPTION} />
+
         <title>{constants.SITE_TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+        <!-- Custom CSS -->
+        <link href="css/freelancer.min.css?cache=4" rel="stylesheet" />
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Play:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
       </Head>
 
       <main className={styles.main}>
