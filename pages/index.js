b7d575617624ec9e407287bba75144701d764c43
Chris Hardcastle <me@chrishardcastle.co.uk>
2020-08-15 19:17:50 +0100

    Added site title, favicon and deleted unused icon

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import constants from '../src/constants';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{constants.SITE_TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
