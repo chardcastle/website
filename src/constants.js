8239a0805bd529e23919cc27ef2abcb66d2ed787
Chris Hardcastle <me@chrishardcastle.co.uk>
2020-08-18 18:43:43 +0100

    Added file excluded by accident in the last commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const myTitle = 'Chris Hardcastle';
+
+exports.SITE_TITLE = `${myTitle} - Software Development Engineer`;
+exports.SITE_DESCRIPTION = `${myTitle} is a Software Development Engineer from the UK`;
+exports.SITE_AUTHOR = myTitle;
+
+exports.OG_TITLE = myTitle;
+exports.OG_TYPE = 'website';
+exports.OG_URL = 'http://chrishardcastle.co.uk';
+exports.OG_IMAGE = 'http://www.chrishardcastle.co.uk/img/profile.png';
+exports.OG_DESCRIPTION = `${myTitle} is a senior software developer from the UK`;
+
+exports.IE_SUPPORT = '<!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->';
