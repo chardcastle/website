a2ca8207c0572e21434b28d74d3d516e0b530887
Chris Hardcastle <me@chrishardcastle.co.uk>
2020-09-01 10:36:45 +0100

    Renamed files to appease agreeable rule

--- src/components/experienceItem.jsx
+++ src/components/experienceItem.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
-import styles from "./experienceItem.module.css";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-const cx = require('classnames');
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import cx from 'classnames';
+import styles from './experienceItem.module.css';
 
 function ExperienceItem(props) {
   const { experience } = props;
@@ -23,32 +24,45 @@ function ExperienceItem(props) {
           <img src={experience.thumbnail} className="img-responsive" alt="" />
         </a>
       </div>
-      <Modal show={show} onHide={handleClose}
-             dialogClassName={styles.experienceDescription}
-             animation={false}>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        dialogClassName={styles.experienceDescription}
+        animation={false}
+      >
         <Modal.Header closeButton>
           <Modal.Title>{experience.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {experience.paragraphs && experience.paragraphs.map((paragraph, i) => {
-            return (<p>{paragraph}</p>)
-          })}
+          {experience.paragraphs && experience.paragraphs.map((paragraph) => (
+            <p>{paragraph}</p>
+          ))}
           <br />
-          {experience.lists && experience.lists.map((list, i) => {
-            return (<ul>
+          {experience.lists && experience.lists.map((list, i) => (
+            <ul>
               <li><strong>{list.listTitle}</strong></li>
               <li>{list.listValues}</li>
-            </ul>)
-          })}
+            </ul>
+          ))}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            <i className="fa fa-times" /> Close
+            <i className="fa fa-times" />
+            {' '}
+            Close
           </Button>
         </Modal.Footer>
       </Modal>
     </>
-  )
+  );
 }
+
+ExperienceItem.propTypes = {
+  experience: PropTypes.objectOf({
+    title: PropTypes.string,
+    lists: PropTypes.array,
+    paragraphs: PropTypes.array,
+  }).isRequired,
+};
 
 export default ExperienceItem;
